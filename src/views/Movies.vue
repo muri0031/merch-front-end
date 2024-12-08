317f10c6ec53df501db187cfdeedf02d8b41cfe8
muri0031 <muri0031@algonquinlive.com>
2024-12-07 21:01:20 -0500

    Change code to add movies

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,13 +1,41 @@
 
 <script>
+import { ALL_MOVIES_API_URL } from '../utils';
+
 export default {
-  name: 'Movies',
-}
+  data() {
+    return {
+      movies: [] 
+    };
+  },
+  mounted() {
+    this.fetchMovies(); 
+  },
+  methods: {
+    async fetchMovies() {
+      try {
+        const response = await fetch(ALL_MOVIES_API_URL);
+        const data = await response.json();
+        this.movies = data.data; 
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    }
+  }
+};
 </script>
+
 <template>
-    <div>
-      <h1>Movies</h1>
-    </div>
-  </template>
+  <div>
+    <h1>Movies</h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        <img :src="movie.Image.url" :alt="movie.Title" style="width: 200px; height: auto;" />
+        <h2>{{ movie.Title }}</h2> 
+      </li>
+    </ul>
+  </div>
+</template>
+
   <style scoped>
   </style>
